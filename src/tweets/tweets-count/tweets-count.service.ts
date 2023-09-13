a033fb6915f5329933c0f1e02fa06a99935b9330
andreenakashima <andreenakashima@gmail.com>
2023-09-13 16:09:15 -0300

    Added tweet count monitoring

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -1,20 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { Tweet } from '../entities/tweet.entity';
 import { InjectModel } from '@nestjs/sequelize';
 import { Interval } from '@nestjs/schedule';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class TweetsCountService {
+  private limit = 10;
+
   constructor(
     @InjectModel(Tweet)
     private tweetModel: typeof Tweet,
+    @Inject(CACHE_MANAGER)
+    private cacheManager: Cache,
   ) {}
 
   @Interval(5000)
   async countTweets() {
+    console.log('procurando tweets');
+    let offset = await this.cacheManager.get<number>('tweet-offset');
+    offset = offset === undefined ? 0 : offset;
+
+    console.log(`offsets: ${offset}`);
     const tweets = await this.tweetModel.findAll({
-      offset: 0,
-      limit: 10,
+      offset,
+      limit: this.limit,
     });
+
+    console.log(`${tweets.length} encontrados`);
+
+    if (tweets.length === this.limit) {
+      const timeInMinutes: number = 1000 * 60 * 10; // 10min;
+      this.cacheManager.set('tweet-offset', offset + this.limit, timeInMinutes);
+
+      console.log(`Achou mais ${this.limit} tweets`);
+    }
   }
 }
