5f88add8cc131c688b83a460c0dae8cbd561aad5
andreenakashima <andreenakashima@gmail.com>
2023-09-14 11:01:20 -0300

    Added email queue

--- src/tweets/tweets-count/tweets-count.service.ts
+++ src/tweets/tweets-count/tweets-count.service.ts
@@ -4,6 +4,8 @@ import { InjectModel } from '@nestjs/sequelize';
 import { Interval } from '@nestjs/schedule';
 import { Cache } from 'cache-manager';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Queue } from 'bull';
+import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
 export class TweetsCountService {
@@ -14,6 +16,8 @@ export class TweetsCountService {
     private tweetModel: typeof Tweet,
     @Inject(CACHE_MANAGER)
     private cacheManager: Cache,
+    @InjectQueue('emails')
+    private emailsQueue: Queue,
   ) {}
 
   @Interval(5000)
@@ -35,6 +39,7 @@ export class TweetsCountService {
       this.cacheManager.set('tweet-offset', offset + this.limit, timeInMinutes);
 
       console.log(`Achou mais ${this.limit} tweets`);
+      this.emailsQueue.add({ tweets: tweets.map((t) => t.toJSON()) });
     }
   }
 }
